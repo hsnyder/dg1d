46bc1f11a9f48157cedb388c07c4dd60dce6ea46
Harris M Snyder <harris.snyder@mail.utoronto.ca>
2020-07-06 13:09:08 -0400

    Reverted to GL quadrature points

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Written as self-study. There are a number of inefficiencies. Not recommended for
 Run make to build it, execute ./dg1d, then run ./vis.py to create an .mp4 file with the animated plot.
 Animation requires that ffmpeg is installed (along with python3, numpy, matplotlib).
 
-
+Parameters and domain decomposition logic are all in the source code.
--- dg1d.F90
+++ dg1d.F90
@@ -15,7 +15,7 @@ program dg1d
     !-------------------------------------------------------------------------------------------------- !
     !   PARAMETERS 
     !-------------------------------------------------------------------------------------------------- 
-    integer, parameter :: Np = 6 ! Number of Gauss-Lagrange points (so order is N-1) 
+    integer, parameter :: Np = 9 ! Number of Gauss-Lagrange points (so order is N-1) 
     integer, parameter :: Nel = 5 ! Number of elements in our domain 
     integer, parameter :: Nt = 2000 ! Number of time steps to run for
     integer, parameter :: write_interval = 20 ! write data every x timesteps 
@@ -57,8 +57,8 @@ program dg1d
     elementPtsLR(2,:) = linspace(2:Nel+1)
 
     ! Calculate gaussian quadrature points and weights
-    call GLL_Points(gx) 
-    call GLL_Weights(gx, gw)
+    call GL_Points(gx) 
+    call GL_Weights(gx, gw)
 
 
     ! Initial conditions
@@ -140,81 +140,82 @@ program dg1d
     
 
 contains
-! GL version
-!   pure function qprime_el(Nel, Np, gx, stiff, massinv, lineJac, c, q, i )
-!       integer, intent(in) :: Nel, Np, i ! i is the ith element
-!       real(real64), intent(in) :: gx(Np), stiff(Np,Np)
-!       real(real64), intent(in) :: q(Np,Nel), massinv(Np,Np), lineJac(Np)
-!       real(real64), intent(in) :: c
-!       real(real64) :: qprime_el(Np)
-!
-!       real(real64) :: x_L, x_R ! "left" and "right" element boundaries, in [-1,1] (i.e. either -1 or 1)
-!
-!       integer :: j ! Iterators, no special meaning
-!
-!       integer :: iPrev   ! Index of previous element
-!       real(real64) :: qR(Np), qRprev(Np)  ! q-vector on the right endpt of this elmnt and upwind elmnt
-!       real(real64) :: flx(Np) ! flux-vector
-!
-!       ! To accomodate our upwind difference scheme for the flux, we need to know whether c is positive 
-!       if (c >= 0) then
-!           ! Apply periodic BC, set iPrev to be the upwind element
-!           iPrev = i-1
-!           if (iPrev == 0) iPrev = Nel
-!           x_L = -1.0_real64
-!           x_R = 1.0_real64
-!       else 
-!           ! Apply periodic BC, set iPrev to be the upwind element
-!           ! if c is negative we swap xL and xR because we need to evaluate at the opposite element boundary
-!           iPrev = i+1
-!           if (iPrev > Nel) iPrev = 1
-!           x_L = 1.0_real64
-!           x_R = -1.0_real64
-!       end if 
-!
-!
-!       ! Populate flx
-!       ! Upwind difference 
-!       ! Flux = loss - gain
-!       qR     = sum([( Lagrange(gx,j,x_R)  * q(j,i)      , j=1, Np )])
-!       qRprev = sum([( Lagrange(gx,j,x_R)  * q(j,iPrev)  , j=1, Np )])
-!
-!       flx = abs(c)* [(  qR(j) * Lagrange(gx,j,x_R) - qRprev(j) * Lagrange(gx,j,x_L) ,j=1,Np)]
-!
-!       ! Calculate qprime
-!       qprime_el = lineJac * matmul(massinv, (c * matmul(stiff,q(:,i)) - flx) )
-!
-!   end function
-
-
-    pure function qprime_el(Nel, Np, gx, stiff, massinv, lineJac, cBkwd, cFwd, q , prevBdyVal, nextBdyVal, i )
+    ! GL version
+    pure function qprime_el(Nel, Np, gx, stiff, massinv, lineJac, c, q, i )
         integer, intent(in) :: Nel, Np, i ! i is the ith element
         real(real64), intent(in) :: gx(Np), stiff(Np,Np)
-        real(real64), intent(in) :: q(Np), massinv(Np,Np), lineJac(Np), prevBdyVal, nextBdyVal
-        real(real64), intent(in) :: cFwd, cBkwd
+        real(real64), intent(in) :: q(Np,Nel), massinv(Np,Np), lineJac(Np)
+        real(real64), intent(in) :: c
         real(real64) :: qprime_el(Np)
-
+ 
+        real(real64) :: x_L, x_R ! "left" and "right" element boundaries, in [-1,1] (i.e. either -1 or 1)
+ 
         integer :: j ! Iterators, no special meaning
-
+ 
+        integer :: iPrev   ! Index of previous element
+        real(real64) :: qR(Np), qRprev(Np)  ! q-vector on the right endpt of this elmnt and upwind elmnt
         real(real64) :: flx(Np) ! flux-vector
-
-                ! Populate flx
+ 
+        ! To accomodate our upwind difference scheme for the flux, we need to know whether c is positive 
+        if (c >= 0) then
+            ! Apply periodic BC, set iPrev to be the upwind element
+            iPrev = i-1
+            if (iPrev == 0) iPrev = Nel
+            x_L = -1.0_real64
+            x_R = 1.0_real64
+        else 
+            ! Apply periodic BC, set iPrev to be the upwind element
+            ! if c is negative we swap xL and xR because we need to evaluate at the opposite element boundary
+            iPrev = i+1
+            if (iPrev > Nel) iPrev = 1
+            x_L = 1.0_real64
+            x_R = -1.0_real64
+        end if 
+ 
+ 
+        ! Populate flx
         ! Upwind difference 
         ! Flux = loss - gain
-        
-        flx = 0
-        if (cFwd>=0) then ! losing at top end, gaining at lower end
-            flx(1) = cBkwd*prevBdyVal
-            flx(Np) = cFwd*q(Np)
-        else ! losing at bottom end, gaining at top end
-            flx(1) = cBkwd*q(1)
-            flx(Np) = cFwd*nextBdyVal
-        end if
-
+        qR     = sum([( Lagrange(gx,j,x_R)  * q(j,i)      , j=1, Np )])
+        qRprev = sum([( Lagrange(gx,j,x_R)  * q(j,iPrev)  , j=1, Np )])
+ 
+        flx = abs(c)* [(  qR(j) * Lagrange(gx,j,x_R) - qRprev(j) * Lagrange(gx,j,x_L) ,j=1,Np)]
+ 
         ! Calculate qprime
-        qprime_el = lineJac * matmul(massinv, (cFwd * matmul(stiff,q) - flx) )
-
+        qprime_el = lineJac * matmul(massinv, (c * matmul(stiff,q(:,i)) - flx) )
+ 
     end function
+
+
+!    ! GLL version
+!    pure function qprime_el(Nel, Np, gx, stiff, massinv, lineJac, cBkwd, cFwd, q , prevBdyVal, nextBdyVal, i )
+!        integer, intent(in) :: Nel, Np, i ! i is the ith element
+!        real(real64), intent(in) :: gx(Np), stiff(Np,Np)
+!        real(real64), intent(in) :: q(Np), massinv(Np,Np), lineJac(Np), prevBdyVal, nextBdyVal
+!        real(real64), intent(in) :: cFwd, cBkwd
+!        real(real64) :: qprime_el(Np)
+!
+!        integer :: j ! Iterators, no special meaning
+!
+!        real(real64) :: flx(Np) ! flux-vector
+!
+!                ! Populate flx
+!        ! Upwind difference 
+!        ! Flux = loss - gain
+!        
+!        flx = 0
+!        if (cFwd>=0) then ! losing at top end, gaining at lower end
+!            flx(1) = cBkwd*prevBdyVal
+!            flx(Np) = cFwd*q(Np)
+!        else ! losing at bottom end, gaining at top end
+!            flx(1) = cBkwd*q(1)
+!            flx(Np) = cFwd*nextBdyVal
+!        end if
+!
+!        ! Calculate qprime
+!        qprime_el = lineJac * matmul(massinv, (cFwd * matmul(stiff,q) - flx) )
+!
+!    end function
 
 
 
@@ -242,16 +243,20 @@ contains
         do i = 1, Nel
             delta_x = elementPtsLR(2,i) - elementPtsLR(1,i)
             Jac = [(2.0/(delta_x), i = 1, Np)]
-            ! To accomodate our upwind difference scheme for the flux, we need to know whether c is positive 
-            iNext = i+1
-            iPrev = i-1
 
-            ! Periodic BC
-            if(iNext > Nel) iNext = 1
-            if(iPrev < 1) iPrev = Nel
+            qprime(:,i) = qprime_el(Nel, Np, gx, stiff, massinv1d, Jac, c, q, i)
 
-            ! Calculate qprime
-            qprime(:,i) = qprime_el(Nel, Np, gx, stiff, massinv1d, Jac, -c, c, q(:,i), q(Np,iPrev), q(1,iNext), i)
+            ! GLL Version
+            !! To accomodate our upwind difference scheme for the flux, we need to know whether c is positive 
+            !iNext = i+1
+            !iPrev = i-1
+
+            !! Periodic BC
+            !if(iNext > Nel) iNext = 1
+            !if(iPrev < 1) iPrev = Nel
+
+            !! Calculate qprime
+            !qprime(:,i) = qprime_el(Nel, Np, gx, stiff, massinv1d, Jac, -c, c, q(:,i), q(Np,iPrev), q(1,iNext), i)
         end do
     end subroutine calc_qprime
 
